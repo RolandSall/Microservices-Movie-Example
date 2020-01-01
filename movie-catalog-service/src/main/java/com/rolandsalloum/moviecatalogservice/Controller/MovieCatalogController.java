5a202a0521dde4cca856b5968f6e99f0f4b3dcfd
Roland Salloum <roland.salloum00@outlook.com>
2020-01-01 18:29:17 +0200

    Added Swagger Config + Connection to database using dependencies hibernate, JPA and MySQL connector

---
+++ movie-catalog-service/src/main/java/com/rolandsalloum/moviecatalogservice/Controller/MovieCatalogController.java
@@ -0,0 +1,12 @@
+package com.rolandsalloum.moviecatalogservice.Controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MovieCatalogController {
+
+
+
+
+}
