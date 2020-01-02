df6c21858906977182fd8840275807aafaa7a943
Roland Salloum <roland.salloum00@outlook.com>
2020-01-02 15:44:55 +0200

    Fixing configuration and adding annotation

--- rating-data-service/src/main/java/com/rolandsalloum/ratingdataservice/RatingDataServiceApplication.java
+++ rating-data-service/src/main/java/com/rolandsalloum/ratingdataservice/RatingDataServiceApplication.java
@@ -2,8 +2,10 @@ package com.rolandsalloum.ratingdataservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+
 public class RatingDataServiceApplication {
 
 	public static void main(String[] args) {
